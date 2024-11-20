ff30fc35c46fef4e88f84fb47b39e33aec1670c7
Sudalav <mcudav240@gmail.com>
2024-11-20 20:35:58 +0300

    added Chess: Chess Board

--- ChessBoard.cpp
+++ ChessBoard.cpp
@@ -1,7 +1,6 @@
 //
 // Created by Sudalav Makakovich on 16.11.2024.
 //
-#include "PiecesChess.h"
 #include "ChessBoard.h"
 #include <iostream>
 
--- ChessBoard.h
+++ ChessBoard.h
@@ -13,25 +13,71 @@ private:
   char** chessBoard;
 public:
   ChessBoard() {
-    chessBoard = new char* [8];
-    for (int i = 0; i < 8; ++i) {
-      chessBoard[i] = new char [8];
-      for (int j = 0; j < 8; ++j) {
-        chessBoard[i][j] = '.';
+    chessBoard = new char* [10];
+    for (int i = 0; i < 10; i++) {
+      chessBoard[i] = new char [10];
+      for (int j = 0; j < 10; j++) {
+        if((i + j) % 2 == 0) {
+          chessBoard[i][j] = '.';
+        } else {
+          chessBoard[i][j] = ' ';
+        }
       }
     }
+    for(int i = 0; i < 10; i++) {
+      for(int j = 0; j < 10; j++) {
+        if(i == 0) {
+          if(j > 0) {
+            chessBoard[j][i] = '9' - j;
+          }
+          if(j > 7) {
+            break;
+          }
+        }
+        if(i == 9) {
+          if(j > 0) {
+            chessBoard[j][i] = '9' - j;
+          }
+          if(j > 7) {
+            break;
+          }
+        }
+      }
+    }
+    for(int i = 0; i < 10; i++) {
+      for(int j = 0; j < 10; j++) {
+        if(i == 0) {
+          if(j > 0) {
+            chessBoard[i][j] =  '`' + j;
+          }
+          if(j > 7) {
+            break;
+          }
+        }
+        if(i == 9) {
+          if(j > 0) {
+            chessBoard[i][j] =  '`' + j;
+          }
+          if(j > 7) {
+            break;
+          }
+        }
+      }
+    }
+    chessBoard[0][0] = ' ';
+    chessBoard[9][9] = ' ';
   }
 
   ~ChessBoard() {
-    for (int i = 0; i < 8; ++i) {
+    for (int i = 0; i < 10; i++) {
       delete[] chessBoard[i];
     }
     delete[] chessBoard;
   }
 
-  void showChessBoard() const {
-    for (int i = 0; i < 8; ++i) {
-      for (int j = 0; j < 8; ++j) {
+  void showChessBoard() {
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
         cout << chessBoard[i][j] << " ";
       }
       cout << endl;
